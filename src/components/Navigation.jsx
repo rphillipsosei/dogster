6eea83c8fe2336a0c5e0a899a0f4d14aef00a993
Rhoda P <rhodaphillipsosei@gmail.com>
2022-07-18 19:50:15 -0400

    import login modal

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,13 +3,17 @@ import styled from "styled-components";
 import React from "react";
 import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Login from "./Login.jsx"
+import Register from "./Register.jsx"
 
 export default function Navigation() {
-  const [openLogin, setOpenLogin] = useState();
-
-  const clickHandler = () => {
-    
-  }
+  const [openLog, setOpenLog] = React.useState(false);
+  const openLogin = () => setOpenLog(true);
+  const closeLogin = () => setOpenLog(false);
 
   return (
     <Nav>
@@ -17,8 +21,25 @@ export default function Navigation() {
         <img src={logo} height={70} width={70} />
       </Logo>
       <ButtonContainer>
-        <NavButton>LOG IN</NavButton>
-        <NavButton>REGISTER</NavButton>
+        <NavButton
+          onClick={openLogin}>LOG IN</NavButton>
+          <Modal
+        open={openLog}
+        onClose={closeLogin}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+        <NavButton
+         >REGISTER</NavButton>
       </ButtonContainer>
     </Nav>
   );
@@ -72,3 +93,16 @@ const NavButton = styled.button`
   font-weight: bold;
   margin-right: 0.5rem;
 `;
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
